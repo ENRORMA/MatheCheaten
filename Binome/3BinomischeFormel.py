db850e5ed07c4a70da8877195c4a4c0aae5dcec6
enrorma <enrorma@gmail.com>
2023-10-01 03:10:49 +0200

    fixed 2 and added 3

---
+++ Binome/3BinomischeFormel.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+import random
+print("Die dritte binomische formel ist:")
+print("  (a + b) * (a - b)")
+print(f"= a\u00b2 - b\u00b2\n\n")
+
+Counter = 1#int(input("Wie Viele: "))
+
+while Counter != 0:
+	Counter += -1
+	a = 2#random.randrange(2,20)
+	b = 4#random.randrange(2,20)
+	foo = 0#random.randrange(0,2)
+	if foo == 0:
+		FirstIsX = True
+	else:
+		FirstIsX = False
+
+	if FirstIsX == True:
+		print(f"({a}x + {b}) * ({a}x - {b})")
+		#print(f"{a}\u00b2 - {a}*{b} + {a}*{b} - {b}\u00b2")
+		print(f"{a*a}x\u00b2 - {b*b}")
+	else:
+		print(f"({a} + {b}x) * ({a} - {b}x)")
+		print(f"{a*a} - {b*b}x\u00b2")
+	print("-------------------------------------------------------")
